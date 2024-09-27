7024d10eba7a68140cd3015a2923dc8d802aad05
Javid Ibra <mr_javid95@mail.ru>
2024-09-27 17:53:33 +0400

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/screens/auth.dart';
+import 'package:flutter_chat_app/screens/chat.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const App());
@@ -7,6 +11,60 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
+
+  // Token kontrolü ve Stream oluşturma
+  Stream<bool> checkTokenStream() async* {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('jwt_token');
+
+    if (token != null) {
+      var decodedToken = parseJwt(token);
+      DateTime expiryDate =
+          DateTime.fromMillisecondsSinceEpoch(decodedToken['exp'] * 1000);
+
+      if (expiryDate.isAfter(DateTime.now())) {
+        yield true;
+      } else {
+        yield false;
+      }
+    } else {
+      yield false;
+    }
+  }
+
+  Map<String, dynamic> parseJwt(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      throw Exception('invalid token');
+    }
+
+    final payload = _decodeBase64(parts[1]);
+    final payloadMap = json.decode(payload);
+    if (payloadMap is! Map<String, dynamic>) {
+      throw Exception('invalid payload');
+    }
+
+    return payloadMap;
+  }
+
+  String _decodeBase64(String str) {
+    String output = str.replaceAll('-', '+').replaceAll('_', '/');
+
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw Exception('Illegal base64url string!"');
+    }
+
+    return utf8.decode(base64Url.decode(output));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +75,20 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder<bool>(
+        stream: checkTokenStream(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasData && snapshot.data == true) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
