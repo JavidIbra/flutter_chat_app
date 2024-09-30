a517b7c15a2d6755639234636d8e4432dc1b6b6e
Javid Ibra <mr_javid95@mail.ru>
2024-09-30 08:44:59 +0400

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ import 'package:flutter_chat_app/screens/auth.dart';
 import 'package:flutter_chat_app/screens/chat.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
